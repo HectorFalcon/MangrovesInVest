3488653ceffef2b706fa209090557b35b7886864
eleonBM <leonsol.eduardo@gmail.com>
2022-08-25 13:07:46 -0600

    New Scripts

--- 02-san_quintin_rasterize.R
+++ 02-san_quintin_rasterize.R
@@ -6,22 +6,26 @@ shp <- list.files("data/san_quintin/adjusted/", pattern=".shp",
                   recursive=T,
                   full.names=T)
 
+
+
 make.raster <- function(x){
   dir.create("data/san_quintin/raster/", showWarnings = F)
   path="data/san_quintin/raster/"
   
-  ext <- st_read("data/study_area/san_quintin.shp")
+  ext <- st_read("data/study_area/ramsar_site_sq.shp") %>% 
+    st_transform(., crs=32611)
   
   ext <- raster::extent(ext)
   
-  gridsize <- 0.0001
+  gridsize <- 100
   
   r <- raster::raster(ext, res=gridsize)
        
-  y <- read_sf(x)
+  y <- st_read(x)%>% 
+    st_transform(., crs=32611) 
   
   rast <- raster::rasterize(y, r, field= "class")
-  crs(rast) <- "+proj=longlat"
+  crs(rast) <- "+proj=utm +zone=11 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
   
   raster::writeRaster(rast,
                       paste0(str_remove(
@@ -35,3 +39,7 @@ make.raster <- function(x){
 }
 
 lapply(shp, make.raster)
+
+
+r <- raster("data/san_quintin/raster/uso-de-suelo-vegetacion_serie_VII_adj.tif")
+plot(r)
